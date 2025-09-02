fd78a3b894480503d88d9e61178e68c63066067e
Jhonnety <jhonestebanvelasquezgomez@gmail.com>
2025-09-02 14:24:50 -0500

    Fix bug viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SiteNavbar } from "@/components/lokl-academy/layouts/site-navbar";
@@ -91,15 +91,16 @@ export const metadata: Metadata = {
       },
     }),
   },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
   verification: {
     google: "google-site-verification-code",
     yandex: "yandex-verification-code",
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
