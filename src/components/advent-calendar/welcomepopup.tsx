9f22898393c0fa6403d2b97aafa3f9137287603c
SebasGuzmanLOKL <sebastianguzman@lokl.life>
2025-12-11 14:56:51 -0500

    fix welcome popup

--- src/components/advent-calendar/welcomepopup.tsx
+++ src/components/advent-calendar/welcomepopup.tsx
@@ -22,7 +22,7 @@ export function WelcomePopup({ onOpenRegistration }: WelcomePopupProps) {
     useEffect(() => {
         const calculateStatus = () => {
             const now = new Date();
-            const startDate = new Date('2025-12-10T00:00:00');
+            const startDate = new Date('2025-12-13T00:00:00');
             const endDate = new Date('2025-12-22T23:59:59');
 
             if (now < startDate) {
